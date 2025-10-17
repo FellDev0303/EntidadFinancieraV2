9049686e561a516f27c433941a90ec44674dc063
FellDev0303 <josejose2868@hotmail.com>
2025-10-17 14:57:31 -0500

    Se crea la clase ControladorCuenta.java que se encargará de solicitar los datos necesarios para realizar los métodos

---
+++ src/main/java/Control/ControladorCuenta.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Control;
+
+import javax.swing.JOptionPane;
+import Modelo.*;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class ControladorCuenta {
+
+    private Cliente cliente;
+
+    public ControladorCuenta(Cliente cliente) {
+        this.cliente = cliente;
+    }
+}
