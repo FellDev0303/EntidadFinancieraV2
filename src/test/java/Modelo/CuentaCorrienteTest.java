9135ee730df8f402ac1c4abcb827d887568c3835
veronz07 <veronica.nunez@uao.edu.co>
2025-10-17 15:11:06 -0500

    se hace el test de la clase CuentaCorriente en donde se verifica que todos los métodos funcionen correctamente

---
+++ src/test/java/Modelo/CuentaCorrienteTest.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package Modelo;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author Vero
+ */
+public class CuentaCorrienteTest {
+
+    @Test
+    public void testRetirarConSobregiro() {
+        CuentaCorriente cuenta = new CuentaCorriente(100, "456", 0, "Verónica", "16/10/2025");
+        cuenta.consignar(10);
+        cuenta.retirar(50); 
+        assertEquals(60, cuenta.getCupoSobregiro());
+    }
+
+    @Test
+    public void testConsignarSobregiro() {
+        CuentaCorriente cuenta = new CuentaCorriente(100, "456", 0, "Verónica", "16/10/2025");
+        cuenta.consignarSobregiro(50);
+        assertEquals(100, cuenta.getCupoSobregiro());
+    }
+
+}
