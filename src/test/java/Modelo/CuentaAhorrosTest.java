47f3254ff71c2531e15aa9ec96a64860e0ddf855
veronz07 <veronica.nunez@uao.edu.co>
2025-10-17 15:13:05 -0500

    se hace el test de la clase CuentaAhorros en donde se verifica que todos los métodos funcionen correctamente

---
+++ src/test/java/Modelo/CuentaAhorrosTest.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package Modelo;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author Vero
+ */
+public class CuentaAhorrosTest {
+
+    @Test
+    public void testConsignar() {
+        CuentaAhorros cuenta = new CuentaAhorros(0.02, "123", 0, "José", "16/10/2025");
+        cuenta.consignar(50000);
+        assertEquals(50000, cuenta.getSaldoCuenta());
+    }
+
+    @Test
+    public void testRetirar() {
+        CuentaAhorros cuenta = new CuentaAhorros(0.02, "123", 0, "José", "16/10/2025");
+        cuenta.consignar(30000);
+        cuenta.retirar(10000);
+        assertEquals(20000, cuenta.getSaldoCuenta());
+    }
+
+}
